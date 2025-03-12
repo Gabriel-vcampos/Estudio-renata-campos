8826e502d6b390f4eca8730984bab47bb422dd8a
Gabriel <137214650+Gabriel-vcampos@users.noreply.github.com>
2025-03-11 21:59:31 -0300

    Commit finalmente nave barra 100% concluida

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+
+// Funções do Carrossel de Frutas
+document.querySelectorAll('.fruit-item').forEach(item => {
+    item.addEventListener('click', function() {
+        document.querySelectorAll('.fruit-item').forEach(i => i.classList.remove('active'));
+        this.classList.add('active');
+        
+        document.querySelectorAll('.content-panel').forEach(panel => {
+            panel.classList.remove('active');
+        });
+        
+        const target = this.getAttribute('data-fruit');
+        document.getElementById(target).classList.add('active');
+    });
+});
+
+// Funções da Sidebar
+const sidebar = document.getElementById('sidebar');
+const overlay = document.getElementById('overlay');
+const setMenu = document.querySelector('.set-menu');
+
+setMenu.addEventListener('click', () => {
+    sidebar.classList.toggle('active');
+    overlay.classList.toggle('active');
+    
+    // Controle da imagem da sidebar
+    const sidebarImage = document.querySelector('.sidebar-image');
+    if (sidebar.classList.contains('active')) {
+        sidebarImage.style.display = 'block';
+    } else {
+        setTimeout(() => {
+            sidebarImage.style.display = 'none';
+        }, 300);
+    }
+});
+
+overlay.addEventListener('click', () => {
+    sidebar.classList.remove('active');
+    overlay.classList.remove('active');
+});
